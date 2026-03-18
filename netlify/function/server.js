826936a5b6eb38444c9a09a28763f98d02c4e758
leulenq <>
2026-03-18 15:53:36 -0400

    toml

--- netlify/function/server.js
+++ netlify/function/server.js
@@ -1,17 +1,3 @@
-// nft cannot resolve express's internal directory requires at trace time.
-// Explicitly requiring each internal module via absolute package paths
-// forces nft to include them in the function bundle.
-require('express/lib/router');
-require('express/lib/router/route');
-require('express/lib/router/layer');
-require('express/lib/middleware/init');
-require('express/lib/middleware/query');
-require('express/lib/request');
-require('express/lib/response');
-require('express/lib/utils');
-require('express/lib/view');
-require('express/lib/application');
-
 const serverless = require('serverless-http');
 const app = require('../../src/app');
 
